4b32a1fc2ffcaab66f86b3ab40847c9daa9c8a22
jimmyhuynh2000 <jimmyhuynh2000@vt.edu>
2023-01-02 01:24:20 -0500

    setup stuff

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
+import About from './pages/About';
+import ContactForm from './pages/Contact';
+import NoPage from './pages/NoPage';
+import Header from './components/Header';
+import Box from '@mui/material/Box';
+import React from 'react';
+import Home from './pages/Home';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    {/* <Header/> */}
+    <Box sx={{ display: 'flex' }}>
+       <Box component="main" sx={{ flexGrow: 1 }}>
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          {/* <Route exact path="/createRoom" element={<CreateRoom socket={socket} />} /> */}
+          {/* <Route exact path="/joinRoom" element={<JoinRoom socket={socket} />} /> */}
+          <Route exact path="/about" element={<About />} />
+          {/* <Route exact path="/chatOptions" element={<ComplexGrid socket={socket}/>} /> */}
+          <Route exact path="/contact" element={<ContactForm />} />
+          {/* <Route exact path="/room/:id" element={<Room socket={socket} userName={'temp'} />} /> */}
+          <Route exact path="/*" element={<NoPage />} />
+        </Routes>
+      </Box>
+    </Box>
+  </div>
   );
 }
 
---
+++ src/pages/home.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+
+// import '../css/pages/Home.css';
+
+/*
+Join Room page component
+ */
+const Home = props => {
+  let currentDate = new Date();
+  let userName = 'user' + currentDate.getHours() + currentDate.getMinutes() + currentDate.getSeconds();
+
+  Home.propTypes = {
+    socket: PropTypes.object
+  };
+  return (
+    <div className="Home">
+      <Stack direction="row" justifyContent="center" alignItems="center">
+        <Card sx={{ m: 10, textDecoration: 'none', bgcolor: '#10131F' }}>
+          <Box display="flex" justifyContent="center" alignItems="center" sx={{ m: 3, mt: 5, ml: 5, mr: 5 }}>
+            <CardContent>
+              <Typography variant="h4" color="white" gutterBottom>
+                Reconnect or Explore new conversations with friends accross the web
+              </Typography>
+            </CardContent>
+          </Box>
+          <Box display="flex" justifyContent="center" sx={{ mb: 10, alignItems: 'flex-end' }}>
+            <CardActions>
+              <Button
+                sx={{ backgroundColor: '#0440CB', fontWeight: 'bold', color: '#FFFFFF' }}
+                size="large"
+                variant="outlined"
+                href="/chatOptions"
+              >
+                Start exploring
+              </Button>
+            </CardActions>
+          </Box>
+        </Card>
+        {/* <box className="loader">
+                    |<span>o</span><span>_</span><span>o</span>|<span>^</span><span>_</span><span>^</span>|<span>+</span><span>_</span><span>+</span>|
+      </box> */}
+      </Stack>
+    </div>
+  );
+};
+export default Home;
